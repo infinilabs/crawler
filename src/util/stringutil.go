6c169725cf911117b0cdf6f342102850741bf6d1
medcl <m@medcl.net>
2016-02-08 11:21:39 +0800

    add substring function to stringuil

--- src/util/stringutil.go
+++ src/util/stringutil.go
@@ -17,3 +17,24 @@ func ContainStr(s, substr string) bool {
 func StringToUTF16(s string) []uint16 {
 	return utf16.Encode([]rune(s + "\x00"))
 }
+
+func SubString(str string,begin,length int) (substr string) {
+	// 将字符串的转换成[]rune
+	rs := []rune(str)
+	lth := len(rs)
+
+	// 简单的越界判断
+	if begin < 0 {
+		begin = 0
+	}
+	if begin >= lth {
+		begin = lth
+	}
+	end := begin + length
+	if end > lth {
+		end = lth
+	}
+
+	// 返回子串
+	return string(rs[begin:end])
+}
