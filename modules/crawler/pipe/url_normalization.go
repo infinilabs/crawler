5a328ed04074a2acf98bf34ae850b166d55d3efa
medcl <m@medcl.net>
2017-06-13 09:21:09 +0800

    limit max length of filename

--- modules/crawler/pipe/url_normalization.go
+++ modules/crawler/pipe/url_normalization.go
@@ -27,6 +27,7 @@ import (
 	"sort"
 	"strings"
 	"time"
+	"github.com/medcl/gopa/core/errors"
 )
 
 const UrlNormalization JointKey = "url_normalization"
@@ -35,6 +36,7 @@ type UrlNormalizationJoint struct {
 	timeout             time.Duration
 	splitByUrlParameter []string
 	FollowSubDomain     bool
+	maxFileNameLength int
 }
 
 var defaultFileName = "default.html"
@@ -300,6 +302,19 @@ func (this UrlNormalizationJoint) Process(context *Context) error {
 		filePath = path.Join(filePath, filename)
 		filename = defaultFileName
 	}
+
+
+
+	//set default filename limit
+	if(this.maxFileNameLength<=0){
+		this.maxFileNameLength=200;
+	}
+
+	//verify filename
+	if(len(filename)>this.maxFileNameLength){
+		panic(errors.New("file name too long"))
+	}
+
 
 	context.Set(CONTEXT_SAVE_PATH, filePath)
 	context.Set(CONTEXT_SAVE_FILENAME, filename)
