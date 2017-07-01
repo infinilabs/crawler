6ece866affbbed61b0a01557737ccbbbfa26e54f
ycbydd <277262098@qq.com>
2017-07-01 23:02:07 +0800

    remove old nextchecktime strategy

--- modules/crawler/pipe/start.go
+++ modules/crawler/pipe/start.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-   http://www.apache.org/licenses/LICENSE-2.0 
+   http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -67,14 +67,8 @@ func (this InitTaskJoint) Process(context *Context) error {
 		context.ErrorExit("task init error")
 		panic(errors.New("nil task"))
 	}
-	//update last check time
+
 	t1 := time.Now().UTC()
-	task.LastCheckTime = &t1
-
-	nextT := t1.Add(1 * time.Minute)
-	task.NextCheckTime = &nextT
-
-	//update next check time //TODO
 
 	//init snapshot
 	var snapshot = &model.Snapshot{
