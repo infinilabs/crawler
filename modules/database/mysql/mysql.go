0c8c6e1350c3a1df22387c3629ecd5f6d4573294
medcl <m@medcl.net>
2017-08-09 23:26:50 +0800

    update comments

--- modules/database/mysql/mysql.go
+++ modules/database/mysql/mysql.go
@@ -4,10 +4,12 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
+// MySQLConfig defines mysql related config, currently only provide connection, eg: root:password@tcp(127.0.0.1:3306)/gopa?charset=utf8&parseTime=true&loc=Local
 type MySQLConfig struct {
 	Connection string `config:"connection"` //TODO, move to structured config
 }
 
+// GetInstance return mysql instance for further usage
 // before use MySQL, you should create database first
 //DROP DATABASE gopa;
 //CREATE DATABASE IF NOT EXISTS gopa DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
