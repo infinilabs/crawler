81b543df8eb1d6dc95bd92cd15553a1a211927ce
medcl <m@medcl.net>
2017-09-10 20:52:41 +0800

    add test data generator

---
+++ modules/generator/generator.go
@@ -0,0 +1,38 @@
+package generator
+
+import (
+	. "github.com/infinitbyte/gopa/core/config"
+	"github.com/infinitbyte/gopa/core/model"
+	"github.com/infinitbyte/gopa/core/queue"
+	"github.com/infinitbyte/gopa/modules/config"
+	"time"
+)
+
+type GeneratorModule struct {
+}
+
+func (module GeneratorModule) Name() string {
+	return "Generator"
+}
+
+func (module GeneratorModule) Start(cfg *Config) {
+
+	generatorConfig := struct {
+		TaskID  string `config:"task_id"`
+		TaskURL string `config:"task_url"`
+	}{}
+
+	cfg.Unpack(&generatorConfig)
+
+	for {
+		queue.Push(config.CheckChannel, model.NewTaskSeed(generatorConfig.TaskURL, generatorConfig.TaskURL, 0, 0).MustGetBytes())
+		queue.Push(config.FetchChannel, []byte(generatorConfig.TaskID))
+		time.Sleep(100 * time.Millisecond)
+	}
+}
+
+func (module GeneratorModule) Stop() error {
+
+	return nil
+
+}
