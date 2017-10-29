df628cba0bce4b0b866a85debf04afdbd89ebbc8
medcl <m@medcl.net>
2017-10-29 19:11:13 +0800

    redirect parameter urlencode

--- static/assets/js/snapshot_footprint.js
+++ static/assets/js/snapshot_footprint.js
@@ -4,7 +4,7 @@ window.onload = function() {
 
     for (var i = 0; i < anchors.length; i++) {
         if((anchors[i].text))
-        anchors[i].href = "/snapshot/?url=" + anchors[i].href
+        anchors[i].href = "/redirect/?url=" + encodeURIComponent(anchors[i].href)
     }
 }
 
